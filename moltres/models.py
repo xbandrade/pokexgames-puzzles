6b373489a4b00004f3f39c3880129ae8ad4cefa0
Bruno <brunosandrade21@gmail.com>
2023-03-17 20:02:08 -0300

    update requirements

--- moltres/models.py
+++ moltres/models.py
@@ -4,6 +4,3 @@ from django.db import models
 class Pokemon(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'pokedex'
